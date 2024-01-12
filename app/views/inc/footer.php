362527271574cd03378806b9c33c0b997ce82548
cody <codykoscielski@gmail.com>
2024-01-12 11:24:00 -0500

    FEAT: HTMX banner

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,6 @@
+<footer class="d-flex justify-content-center p-2">
+    <img src="<?= URLROOT ?>/images/createdwith.jpeg" alt="Created with HTMX, the right way" width="250">
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="<?= URLROOT ?>/js/htmx.min.js"></script>
 <script src="<?= URLROOT ?>/js/main.js"></script>
